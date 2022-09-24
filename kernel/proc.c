a62f42200472286132e95831666fc967b479d1b3
fatpigeon <wjhhhhhha0@gmail.com>
2022-09-24 15:13:43 +0800

    fixed bugs

--- kernel/proc.c
+++ kernel/proc.c
@@ -165,6 +165,8 @@ freeproc(struct proc *p)
 {
   if(p->trapframe)
     kfree((void*)p->trapframe);
+  if(p->alarm_trapframe)
+    kfree((void*)p->alarm_trapframe);
   p->trapframe = 0;
   if(p->pagetable)
     proc_freepagetable(p->pagetable, p->sz);
